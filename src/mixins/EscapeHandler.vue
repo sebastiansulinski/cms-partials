bc4f73637a757629055bf917338c63963c6dd238
Sebastian Sulinski <info@ssdtutorials.com>
2020-01-06 15:01:47 +0000

    Added EscapeHandler

---
+++ src/mixins/EscapeHandler.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+        created() {
+            const listener = event => {
+                if (event.key === 'Escape') {
+                    this.handleEscape()
+                }
+            }
+            document.addEventListener('keydown', listener)
+            this.$once('hook:beforeDestroy', () => {
+                document.removeEventListener('keydown', listener)
+            })
+        },
+        methods: {
+            handleEscape() {
+            }
+        }
+    }
+</script>
